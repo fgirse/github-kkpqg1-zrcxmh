1c090f894dbfaf63b8e170adafdf31ab9b7573c6
fgirse <fgirse@bluewin.ch>
2022-11-27 03:17:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-kkpqg1-zrcxmh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-kkpqg1-zrcxmh)
---
+++ lib/demos.ts
@@ -0,0 +1,46 @@
+export type Item = {
+  name: string;
+  slug: string;
+  icon: string;
+};
+
+export const demos: {  
+  
+    items: [
+      {
+        name: 'Home',
+        slug: '/',
+        icon: '',
+      },
+      {
+        name: 'Geschichte',
+        slug: 'geschichte',
+        icon: ,
+      },
+      {
+        name: 'Team',
+        slug: 'team',
+        icon:'',
+      },
+      {
+        name:'Drinks',
+        slug: 'drinks',
+        icon: '',
+      },
+      {
+        name: 'Sportarena',
+        slug: 'sportarena',
+        icon: '',
+      },
+      {
+        name: 'Events',
+        slug: 'events',
+        icon: '',
+      },                             
+      {
+        name: 'Wohin ?',
+        slug: 'kontakt',
+        icon: '',
+      }
+    ]
+  }
